3d3923de9eb7f83bd2e4bcd4ee72f35012dba636
Sascha Gaspar <sascha.gaspar@gmx.de>
2022-11-03 16:56:42 +0100

    SSH refactor

--- controllers/sshkey_controller.go
+++ controllers/sshkey_controller.go
@@ -33,19 +33,9 @@ type SshKeyReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SshKey object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+// +kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=hcloud.sva.codes,resources=sshkeys/finalizers,verbs=update
 func (r *SshKeyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
